ce8e9df5d22651ffbf748e628cdff9c82fd2bf29
Serdar Tepeyurt <st@serdartepeyurt.com>
2019-01-18 21:26:24 +0300

    Dependency fixes

--- AddressProviderFramework.Implementations.Ptt.Tester/Program.cs
+++ AddressProviderFramework.Implementations.Ptt.Tester/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Linq;
 
 namespace AddressProviderFramework.Implementations.Ptt.Tester
@@ -10,14 +11,24 @@ namespace AddressProviderFramework.Implementations.Ptt.Tester
             var repo = new PttAddressProviderRepository();
             repo.Initialize("pk_2019_01_14.xlsx");
 
-            // Get first country
+            // Get the first country
             var country = repo.GetCountries().First();
+
+            // Get states
             var states = repo.GetStates(country.Name);
 
             foreach(var state in states)
             {
                 Console.WriteLine(state);
             }
+
+            var sc = new ServiceCollection();
+            sc.AddSingleton<IAddressProviderRepository, PttAddressProviderRepository>(serviceProvider =>
+            {
+                var rep = new PttAddressProviderRepository();
+                repo.Initialize("");
+                return rep;
+            });
 
             Console.ReadLine();
         }
